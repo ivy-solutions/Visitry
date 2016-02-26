3f7234fc524cfbcb50430ced4798596f63859197
Sarah Coletti <sarahcoletti@verizon.net>
2016-02-26 10:33:23 -0500

    Move selector code to list-requests helper

--- server/available-visits.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by sarahcoletti on 2/25/16.
- */
-Meteor.publish("availableVisits", function () {
-  let selector = {
-      'visitorId': {$exists: false}
-  };
-  return Visits.find(selector);
-});
