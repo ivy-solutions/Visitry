0c32a24ec9d4229965d848dfc541e618eff05005
Sarah Coletti <sarahcoletti@verizon.net>
2016-05-12 14:54:39 -0400

    Add hybrid:assets and icons

--- mobile-config.js
+++ mobile-config.js
@@ -12,6 +12,20 @@ App.info({
   website: 'http://visitry.org'
 });
 
+App.icons({
+  // iOS
+  'iphone': 'resources/icons/ios/icon-60.png',
+  'iphone_2x': 'resources/icons/ios/icon-60@2x.png',
+  'iphone_3x': 'resources/icons/ios/icon-60@3x.png',
+  'ipad': 'resources/icons/ios/icon-72.png',
+  'ipad_2x': 'resources/icons/ios/icon-72@2x.png',
+
+  // Android
+  'android_hdpi': 'resources/icons/android/drawable-hdpi/ic_launcher.png',
+  'android_mdpi': 'resources/icons/android/drawable-mdpi/ic_launcher.png',
+  'android_xhdpi': 'resources/icons/android/drawable-xhdpi/ic_launcher.png',
+});
+
 App.accessRule('*.google.com/*');
 App.accessRule('*.googleapis.com/*');
 App.accessRule('*.gstatic.com/*');
