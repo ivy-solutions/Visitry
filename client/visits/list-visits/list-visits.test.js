603890263b42669ab46e05611136021536adf056
Sarah Coletti <sarahcoletti@verizon.net>
2016-06-02 18:27:59 -0400

    Add tests for list-visits

---
+++ client/visits/list-visits/list-visits.test.js
@@ -0,0 +1,55 @@
+/**
+ * Created by sarahcoletti on 6/2/16.
+ */
+/**
+ * Created by sarahcoletti on 5/31/16.
+ */
+import 'angular-mocks';
+import { Meteor } from 'meteor/meteor';
+import { visitry } from '/client/lib/app.js';
+import {chai} from 'meteor/practicalmeteor:chai';
+import { sinon } from 'meteor/practicalmeteor:sinon';
+import '/client/visits/list-visits/list-visits.js';
+
+describe ( 'ListVisits', function() {
+
+  beforeEach(function() {
+    window.module('visitry');
+  });
+
+  describe( 'listVisits', function() {
+    var controller;
+    var findOneStub;
+
+
+    beforeEach(function() {
+       inject( function ($rootScope, $componentController) {
+        controller = $componentController('listVisits', {$scope: $rootScope.$new(true)})
+        });
+      findOneStub = sinon.stub(Meteor.users, 'findOne');
+      findOneStub.returns( { name: 'Harry' });
+
+     });
+
+    afterEach( function() {
+      Meteor.users.findOne.restore();
+    });
+
+    it('3 per page', function () {
+      chai.assert.equal(controller.perPage,3);
+    });
+
+    it('page change', function () {
+      var newPage = { name: 'page 2'};
+      controller.pageChanged(newPage);
+      chai.assert.equal(controller.page.name,'page 2');
+    });
+
+    it ('getUserById', function() {
+      var user = controller.getUserById("anId");
+      findOneStub.calledOnce === true;
+      chai.assert.equal( user.name, "Harry");
+    });
+  });
+});
+
