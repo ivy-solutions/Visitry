efca35298697f6c7ec308a27dc97fc276747cdda
Daniel Biales <daniel.biales@libertymutual.com>
2016-06-08 14:20:11 -0400

    test for ionic popup. This test is currently failing

--- client/visits/pending-visits/pending-visits.test.js
+++ client/visits/pending-visits/pending-visits.test.js
@@ -10,51 +10,62 @@ import StubCollections from 'meteor/hwillson:stub-collections';
 import Visits from '/model/visits.js'
 import myFunctions from '/client/lib/sharedFunctions.js'
 
-describe ( 'Pending Visit Requests', function() {
-  beforeEach(function() {
+describe('Pending Visit Requests', function () {
+  beforeEach(function () {
     angular.mock.module('visitry');
   });
 
-  beforeEach(inject(function(_$controller_){
+  beforeEach(inject(function (_$controller_) {
     // The injector unwraps the underscores (_) from around the parameter names when matching
     $controller = _$controller_;
   }));
 
   var controller;
-  beforeEach(function() {
+  var mockIonicPopup;
+  beforeEach(function () {
     StubCollections.add([Visits]);
     sinon.stub(Meteor.myFunctions, 'dateSortArray');
-    inject( function ($rootScope) {
-      controller = $controller('pendingVisitsCtrl', { $scope: $rootScope.$new(true) });
+    var object = {confirm:function(obj){}};
+    mockIonicPopup = sinon.spy(object,'confirm');
+    inject(function ($rootScope,$ionicPopup) {
+      controller = $controller('pendingVisitsCtrl', {$scope: $rootScope.$new(true), $ionicPopup: mockIonicPopup});
     });
   });
 
-  afterEach(function() {
+  afterEach(function () {
     StubCollections.restore();
     Meteor.myFunctions.dateSortArray.restore();
   });
 
-  describe('list settings',function(){
-    it('show delete icon',function(){
-      chai.assert.equal(controller.showDelete,false);
+  describe('list settings', function () {
+    it('show delete icon', function () {
+      chai.assert.equal(controller.showDelete, false);
     })
-    it('can swipe to delete',function(){
-      chai.assert.equal(controller.canSwipe,true);
+    it('can swipe to delete', function () {
+      chai.assert.equal(controller.canSwipe, true);
     })
-    it('list sorted descending by request day',function(){
+    it('list sorted descending by request day', function () {
       chai.assert.equal(controller.listSort.requestedDate, 1);
     })
   })
 
-  describe('date since requested',function(){
-    it('when date = now, time since requested = 0',function(){
-      chai.assert.equal(controller.getTimeSinceRequested(new Date()),0);
+  describe('date since requested', function () {
+    it('when date = now, time since requested = 0', function () {
+      chai.assert.equal(controller.getTimeSinceRequested(new Date()), 0);
     });
-    it('when date = 5 days from now, time since requested = 5',function(){
-      var date = new Date()
-      date.setDate(date.getDate() - 5)
-      chai.assert.equal(controller.getTimeSinceRequested(date),5)
+    it('when date = 5 days from now, time since requested = 5', function () {
+      var date = new Date();
+      date.setDate(date.getDate() - 5);
+      chai.assert.equal(controller.getTimeSinceRequested(date), 5)
     })
   });
+
+  describe('cancel visit popup', function () {
+    it('when the user cancels a visit the popup is displayed', function () {
+      controller.showCancelVisitConfirm({visitor: '12341'});
+      $rootScope.$digest();
+      chai.assert.equal(mockIonicPopup.confirm.called, true)
+    })
+  })
 
 });
