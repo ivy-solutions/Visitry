3b4a62199575195febbd6601c64a932541e5dc04
Sarah Coletti <sarahcoletti@verizon.net>
2016-08-18 10:19:18 -0400

    Use winston-client on client

--- client/logging.js
+++ client/logging.js
@@ -1,10 +1,14 @@
 /**
  * Created by sarahcoletti on 8/16/16.
  */
-import { Winston } from "meteor/brentjanderson:winston-client"
+import "meteor/brentjanderson:winston-client"
 
-export const logger = {
+var logger = {
+  verbose: Winston.info,  //FIX-ME I have not been able to get verbose or debug to log as such
+  debug: Winston.info,
   info: Winston.info,
   warn: Winston.warn,
   error: Winston.error
 };
+
+export {logger}
