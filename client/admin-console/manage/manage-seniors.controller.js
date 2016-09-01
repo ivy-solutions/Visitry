1613592faf37f1d7ee72230bed077fa2cc4b498b
Daniel Biales <daniel.biales@libertymutual.com>
2016-08-31 22:19:24 -0400

    Added Senior management page. Fixed a number of tests on the server side for visits

---
+++ client/admin-console/manage/manage-seniors.controller.js
@@ -0,0 +1,38 @@
+/**
+ * Created by n0235626 on 8/31/16.
+ */
+
+
+angular.module('visitry.browser').controller('adminManageSeniorsCtrl', function ($scope, $state, $reactive, $cookies) {
+  $reactive(this).attach($scope);
+
+  this.agencyId = $cookies.get('agencyId');
+  this.recordPerPage = 10;
+  this.page = 1;
+  this.sort = {
+    'userData.lastName': -1
+  };
+  this.queryOptions = {
+    limit: parseInt(this.recordPerPage),
+    skip: parseInt((this.page - 1) * this.recordPerPage),
+    sort: this.sort
+  };
+  //FIXME: THis might not work the way I expect
+  this.autorun(() => {
+    this.queryOptions.limit = parseInt(this.getReactively('recordPerPage'));
+    this.queryOptions.skip = parseInt((this.getReactively('page') - 1) * this.recordPerPage);
+    this.queryOptions.sort = this.getReactively('sort');
+  });
+
+  this.subscribe('seniorUsers', ()=> {
+    return [this.getReactively('agencyId'), this.getReactively('queryOptions')]
+  });
+
+  this.helpers({
+    seniors: ()=> {
+      return User.find({'userData.role': {$eq: 'requester'}})
+    }
+  });
+
+
+});
--- server/users.js
+++ server/users.js
@@ -4,13 +4,17 @@ import { Visits } from '/model/visits'
 
 Meteor.publish("userdata", function () {
   if (this.userId) {
-    logger.verbose("publish userdata to " + this.userId );
-    var user = User.findOne({_id: this.userId},{fields: {'userData.agencyId': 1}});
-    return User.find({agencyId: user.userData.agencyId },
-      {fields: {username: 1, emails: 1,
-        'userData.location': 1, 'userData.visitRange': 1,
-        'userData.firstName':1, 'userData.lastName':1,
-        'userData.picture': 1, 'userData.about': 1, 'userData.phoneNumber': 1}});
+    logger.verbose("publish userdata to " + this.userId);
+    var user = User.findOne({_id: this.userId}, {fields: {'userData.agencyId': 1}});
+    return User.find({agencyId: user.userData.agencyId},
+      {
+        fields: {
+          username: 1, emails: 1,
+          'userData.location': 1, 'userData.visitRange': 1,
+          'userData.firstName': 1, 'userData.lastName': 1,
+          'userData.picture': 1, 'userData.about': 1, 'userData.phoneNumber': 1
+        }
+      });
   } else {
     this.ready();
   }
@@ -18,8 +22,8 @@ Meteor.publish("userdata", function () {
 
 Meteor.publish("userProfile", function () {
   if (this.userId) {
-    logger.verbose("publish userProfile to " + this.userId );
-    return User.find({_id:this.userId},
+    logger.verbose("publish userProfile to " + this.userId);
+    return User.find({_id: this.userId},
       {fields: {username: 1, 'userData': 1}});
   } else {
     this.ready();
@@ -58,11 +62,37 @@ Meteor.publish("topVisitors", function (agency, numberOfDays) {
   self.ready();
 });
 
+Meteor.publish("seniorUsers", function (agencyId, options) {
+  if (this.userId) {
+    logger.verbose("publish seniorUsers to " + this.userId);
+    var selector = {
+      'userData.agencyIds': {$elemMatch: {$eq: agencyId}},
+      'userData.role': {$eq: 'requester'}
+    };
+    var queryOptions = {
+      sort: options.sort,
+      skip: options.skip,
+      limit: options.limit,
+      fields: {
+        createdAt: 1,
+        'userData.firstName': 1,
+        'userData.lastName': 1,
+        'userData.picture': 1,
+        'userData.location': 1,
+        'userData.role':1
+      }
+    };
+    return User.find(selector, queryOptions);
+  } else {
+    this.ready();
+  }
+});
+
 Meteor.methods({
   updateName(firstName, lastName, role)
   {
     if (!this.userId) {
-      logger.error( "updateName - user not logged in");
+      logger.error("updateName - user not logged in");
       throw new Meteor.Error('not-logged-in',
         'Must be logged in to update name.');
     }
@@ -71,33 +101,33 @@ Meteor.methods({
     currentUser.userData.lastName = lastName;
     currentUser.userData.role = role;
 
-    currentUser.save(function(err, id) {
+    currentUser.save(function (err, id) {
       if (err) {
         logger.error("updateName failed to update user. err: " + err);
         throw err;
       }
     });
-    logger.info("updateName for userId: " + this.userId );
+    logger.info("updateName for userId: " + this.userId);
   },
   //TODO not used
   updateEmail(email)
   {
     if (!this.userId) {
-      logger.error( "updateEmail - user not logged in");
+      logger.error("updateEmail - user not logged in");
       throw new Meteor.Error('not-logged-in',
         'Must be logged in to update email.');
     }
-    logger.info("updateEmail for userId: " + this.userId );
+    logger.info("updateEmail for userId: " + this.userId);
 
     if (email && Meteor.user().emails) {
       Accounts.removeEmail(this.userId, Meteor.user().emails[0].addresss);
     }
 
-    return Accounts.addEmail(this.userId, email );
+    return Accounts.addEmail(this.userId, email);
   },
   updateLocation(loc) {
     if (!this.userId) {
-      logger.error( "updateLocation - user not logged in");
+      logger.error("updateLocation - user not logged in");
       throw new Meteor.Error('not-logged-in',
         'Must be logged in to update location.');
     }
@@ -115,17 +145,17 @@ Meteor.methods({
     } else {  //removing location
       currentUser.userData.location = null;
     }
-    currentUser.save(function(err, id) {
+    currentUser.save(function (err, id) {
       if (err) {
         logger.error("updateLocation failed to update user. err: " + err);
         throw err;
       }
     });
-    logger.info("updateLocation for userId: " + this.userId );
+    logger.info("updateLocation for userId: " + this.userId);
   },
   updateUserData(data) {
     if (!this.userId) {
-      logger.error( "updateUserData - user not logged in");
+      logger.error("updateUserData - user not logged in");
       throw new Meteor.Error('not-logged-in',
         'Must be logged in to update user data.');
     }
@@ -133,30 +163,30 @@ Meteor.methods({
     currentUser.userData.role = data.role;
     currentUser.userData.visitRange = data.visitRange;
     currentUser.userData.about = data.about;
-    currentUser.userData.phoneNumber = data.phoneNumber ? data.phoneNumber: null; //remove phone number of there is none
-    currentUser.save(function(err, id) {
+    currentUser.userData.phoneNumber = data.phoneNumber ? data.phoneNumber : null; //remove phone number of there is none
+    currentUser.save(function (err, id) {
       if (err) {
         logger.error("updateUserData failed to update user. err: " + err);
         throw err;
       }
     });
-    logger.info("updateUserData for userId: " + this.userId );
+    logger.info("updateUserData for userId: " + this.userId);
   },
   updatePicture(data) {
     if (!this.userId) {
-      logger.error( "updatePicture - user not logged in");
+      logger.error("updatePicture - user not logged in");
       throw new Meteor.Error('not-logged-in',
         'Must be logged in to update picture.');
     }
     var currentUser = User.findOne(this.userId);
     currentUser.userData.picture = data;
-    currentUser.save({fields: ['userData.picture'] },function(err, id) {
+    currentUser.save({fields: ['userData.picture']}, function (err, id) {
       if (err) {
         logger.error("updatePicture failed to update user. err: " + err);
         throw err;
       }
     });
-    logger.info("updatePicture for userId: " + this.userId );
+    logger.info("updatePicture for userId: " + this.userId);
   }
 });
 
@@ -168,13 +198,13 @@ Accounts.onCreateUser(function (options, user) {
   }
   //TODO include real agency in input
   if (!user.userData.agencyId) {  // use default, if no agency selected
-    logger.error( "user has no agency. userId: " + user._id);
-    var agency = Agency.findOne({name:'IVY Agency'});
-    if ( agency ) {
+    logger.error("user has no agency. userId: " + user._id);
+    var agency = Agency.findOne({name: 'IVY Agency'});
+    if (agency) {
       user.userData.agencyIds = [agency._id];
     }
   }
-  logger.info("onCreateUser for userId: " + user._id );
+  logger.info("onCreateUser for userId: " + user._id);
   return user;
 });
 
