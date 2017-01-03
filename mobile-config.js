29336b05561a1c44568af54c66675602021e2e29
Sarah Coletti <sarahcoletti@verizon.net>
2017-01-03 09:24:19 -0500

    warning for users with no agency. Fix test

--- mobile-config.js
+++ mobile-config.js
@@ -5,7 +5,7 @@ App.info({
   id: 'com.ivysolutions.visitry.app',
   name: 'Visitry',
   version: '1.0.14',
-  buildNumber: '100143',
+  buildNumber: '100144',
   description: 'Request and schedule visits through volunteer visiting programs',
   author: 'IvySolutions',
   email: 'admin@visitry.org',
