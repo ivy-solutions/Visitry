4956d20210f4bf88a49687e9b3dcbd5615583afa
Daniel Biales <daniel.biales@libertymutual.com>
2017-01-09 16:50:54 -0500

    This might break -- Adding email templates for Enrollment email

--- server/startup/config.js
+++ server/startup/config.js
@@ -1,5 +1,5 @@
 import { Accounts } from 'meteor/accounts-base'
-
+import { SSR } from 'meteor/meteorhacks:ssr';
 
 Meteor.startup(function () {
   if (Meteor.settings.smtp) {
@@ -10,11 +10,15 @@ Meteor.startup(function () {
     Accounts.emailTemplates.enrollAccount.subject = function (user) {
       return "Welcome to Visitry, " + user.userData.firstName;
     };
-    Accounts.emailTemplates.enrollAccount.text = function (user, url) {
+    SSR.compileTemplate('enrollAccountTemplate',Assets.getText('enroll-account-email.html'));
+    Accounts.emailTemplates.enrollAccount.html = function (user, url) {
+      return SSR.render('enrollAccountTemplate',{user:user,url:url})
+    }
+/*    Accounts.emailTemplates.enrollAccount.text = function (user, url) {
       return "You have been selected to participate in building a better future!"
         + " To activate your account, simply click the link below:\n\n"
         + url;
-    };
+    };*/
     Accounts.emailTemplates.resetPassword.from = function () {
       // Overrides value set in Accounts.emailTemplates.from when resetting passwords
       return "Visitry Password Reset <no-reply@visitry.org>";
@@ -22,10 +26,9 @@ Meteor.startup(function () {
     Accounts.emailTemplates.resetPassword.text = function (user, url) {
       return "Hello,\n\n"
         + " To reset your password, simply click the link below:\n\n"
-        + url +"\n\n"
+        + url + "\n\n"
         + "If you continue to have difficulty signing in to your Visitry.org account, please contact "
         + "mailto:admin@visitry.org ."
     };
-
   }
 });
