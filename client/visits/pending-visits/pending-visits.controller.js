d7fd7716b04c57d6730cc154d481962100626ed7
Sarah Coletti <sarahcoletti@verizon.net>
2017-01-25 10:40:56 -0500

    Fix for incorrect 'not a member' message on initial logon

--- client/visits/pending-visits/pending-visits.controller.js
+++ client/visits/pending-visits/pending-visits.controller.js
@@ -27,8 +27,11 @@ angular.module('visitry').controller('pendingVisitsCtrl',
         requestedDate: {$gte: new Date()}
       }, {sort: this.getReactively('listSort')});
       this.hasRequests = this.visits.count() > 0;
+
       let currentUser = User.findOne(Meteor.userId(),{fields: {'userData.agencyIds': 1, 'userData.prospectiveAgencyIds': 1}});
-      this.hasAgency = currentUser.hasAgency;
+      if ( typeof currentUser.hasAgency !== 'undefined') {
+        this.hasAgency = currentUser.hasAgency;
+      }
     } else {
       feedback.stop()
     }
@@ -39,7 +42,7 @@ angular.module('visitry').controller('pendingVisitsCtrl',
     pendingVisits: ()=> {
       var hasAgency = this.getReactively('hasAgency');
       if (Meteor.userId()) {
-        return Meteor.myFunctions.groupVisitsByRequestedDate(this.visits);
+        return Meteor.myFunctions.groupVisitsByRequestedDate(this.getReactively('visits'));
       } else {
         feedback.stop()
       }
