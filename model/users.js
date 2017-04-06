651c5026ce08e3cf5f018081fa94a27c5aa063eb
Sarah Coletti <sarahcoletti@verizon.net>
2017-04-06 13:38:53 -0400

    remove prospectiveAgencyIds

--- model/users.js
+++ model/users.js
@@ -8,7 +8,6 @@ const UserData = Class.create({
     firstName: {type: String, optional: true},
     lastName: {type: String, optional: true},
     agencyIds: {type: [String], optional: true, index: 1},  //user can initially be unassigned to an agency
-    prospectiveAgencyIds: {type: [String], optional: true},
     about: {type: String, optional: true},
     location: {type: Address, optional:true},
     locationInfo: { type: String, optional: true},
