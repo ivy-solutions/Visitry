0ea5c46ccff101b89e31d0ab1bf331d3c43a73ca
Sarah Coletti <sarahcoletti@verizon.net>
2017-10-19 17:03:43 -0400

    Update mobile build number for redeploy

--- mobile-config.js
+++ mobile-config.js
@@ -5,7 +5,7 @@ App.info({
   id: 'com.ivysolutions.visitry.app',
   name: 'Visitry',
   version: '1.1.5',
-  buildNumber: '101050',
+  buildNumber: '101052',
   description: 'Request and schedule visits through volunteer visiting programs',
   author: 'IvySolutions',
   email: 'admin@visitry.org',
