19be6aba3d81a67cb4dc7a8851d09f8179bd9d56
Daniel Biales <daniel.biales@libertymutual.com>
2018-09-24 20:56:29 -0400

    Checking to see if a change fixes the tests failing.

--- client/lib/sharedFunctions.test.js
+++ client/lib/sharedFunctions.test.js
@@ -4,8 +4,8 @@
 import { Meteor } from 'meteor/meteor';
 import {assert} from 'meteor/practicalmeteor:chai';
 import '/client/lib/sharedFunctions.js';
-import StubPackage from 'meteor/hwillson:stub-collections';
-const StubCollections = StubPackage.default
+import StubCollections from 'meteor/hwillson:stub-collections';
+//let StubCollections = StubPackage.default
 import '/model/users.js'
 import { Roles } from 'meteor/alanning:roles'
 
